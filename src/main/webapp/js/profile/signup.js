8a6b0073459415c2a1d57963e3121ba0121b6f33
AlakayFisher27 <alexfarina175@hotmail.com>
2023-01-03 16:49:15 +0100

    Refresh Azure Connection

--- src/main/webapp/js/profile/signup.js
+++ src/main/webapp/js/profile/signup.js
@@ -14,3 +14,90 @@ function checkNations() {
         });
     }
 }
+
+function signup(type) {
+
+
+    if (type == 'user') {
+
+    } else {
+
+    }
+}
+
+function validateEmail(field) {
+    let div = field.parentElement;
+    div.children[2].innerHTML = "";
+
+    let email = field.value.trim();
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.match(regex)) {
+        return true;
+    }
+    else {
+        div.children[2].innerHTML = "Invalid email format!";
+        return false;
+    }
+}
+
+function validatePassword(field) {
+    let div = field.parentElement;
+    div.children[2].innerHTML = "";
+
+    let password = field.value.trim();
+    let regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if(password.match(regex)) {
+        return true;
+    }
+    else {
+        div.children[2].innerHTML = "Invalid password format!";
+        return false;
+    }
+}
+
+function validateAlias(field) {
+    let div = field.parentElement;
+    div.children[2].innerHTML = "";
+
+    let alias = field.value.trim();
+    if(alias.length >= 10) {
+        return true;
+    }
+    else {
+        div.children[2].innerHTML = "Alias should be at least 10 characters long!";
+        return false;
+    }
+}
+
+function validatePicture(field) {
+    let div = field.parentElement;
+    div.children[3].innerHTML = "";
+
+    let file = field.files[0];
+    let img = new Image();
+
+    let objurl = window.URL.createObjectURL(file);
+    img.onload = function() {
+        if(this.width >= 500 && this.height >= 500) {
+            return true;
+        } else {
+            div.children[3].innerHTML = "Picture should be at least 500x500!";
+            return false;
+        }
+    };
+    img.src = objurl;
+}
+
+function validateBirthdate(field) {
+    let div = field.parentElement;
+    div.children[2].innerHTML = "";
+
+    let birthdate = field.value.trim();
+    if(birthdate != "") {
+        return true;
+    }
+    else {
+        div.children[2].innerHTML = "Enter your birthdate!";
+        return false;
+    }
+}
