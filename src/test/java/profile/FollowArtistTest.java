64f20c3510416d2ac11bef04035937a234ac6bf6
annalindab <a.brenga@studenti.unisa.it>
2023-01-11 11:24:00 +0100

    Upload:FollowArtistTest

---
+++ src/test/java/profile/FollowArtistTest.java
@@ -0,0 +1,50 @@
+package profile;
+
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class FollowArtistTest {
+
+    @Test
+    public void followProfileTest() throws Exception {
+
+        //REQUEST & RESPONSE
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("id")).thenReturn("1");
+
+        //SESSION
+        HttpSession session = mock(HttpSession.class);
+        when(request.getSession()).thenReturn(session);
+
+        //WRITER
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        //PROFILE
+        ProfileDAO profileDAO = mock(ProfileDAO.class);
+        UserBean userBean = mock(UserBean.class);
+        when(userBean.getArtists()).thenReturn(new ArrayList<>());
+        when(session.getAttribute("Profilw")).thenReturn(userBean);
+
+        FollowArtist followArtist = new FollowArtist();
+        followArtist.profileDAO = profileDAO;
+        followArtist.doPost(request,response);
+
+        writer.flush();
+        assert(stringWriter.toString().contains(""));
+    }
+}
